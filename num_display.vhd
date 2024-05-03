787127fb59ff7a2266099d26a24147f45c36b0e7
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-05-03 15:53:31 +0800

    Update num_display.vhd

--- num_display.vhd
+++ num_display.vhd
@@ -1,11 +1,15 @@
+--num_display 输出需要显示的数
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity num_display is
 	port(
 		en: in std_logic;
+		--类似使能端，='0' 输出num，='1'输出空
 		numh, numl: in std_logic_vector(3 downto 0);
+		--输入高四位和低四位
 		dish, disl: out std_logic_vector(3 downto 0)
+		--输出高四位和低四位
 	);
 end num_display;
 

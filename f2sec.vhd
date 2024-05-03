24a11d20ed2e3b2d62806cbb61426b35c02b0fb8
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-05-03 15:51:36 +0800

    Update f2sec.vhd

--- f2sec.vhd
+++ f2sec.vhd
@@ -1,3 +1,4 @@
+--f2sec将10khz的方波转化成1hz空占比为0.8的方波
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
@@ -5,7 +6,9 @@ use ieee.std_logic_unsigned.all;
 entity f2sec is
 	port(
 		clk: in std_logic;
+		--10khz时钟信号
 		nsec: out std_logic
+		--秒
 	);
 end f2sec;
 
@@ -18,11 +21,11 @@ begin
 	begin
 		if(clk'event and clk = '1') then
 			tmp_sec <= tmp_sec + 1;
-			if(tmp_sec >= 5000) then
+			if(tmp_sec >= 10000) then
 				tmp_sec <= 0;
 			end if;
 		end if;
-		if(tmp_sec <= 1000) then
+		if(tmp_sec < 2000) then
 			nsec <= '1';
 		else
 			nsec <= '0';

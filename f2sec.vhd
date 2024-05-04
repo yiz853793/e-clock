35415d9343c1877e2155ffb10d470ee49113c4c2
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-05-04 09:55:21 +0800

    Update f2sec.vhd

--- f2sec.vhd
+++ f2sec.vhd
@@ -6,7 +6,7 @@ use ieee.std_logic_unsigned.all;
 entity f2sec is
 	port(
 		clk: in std_logic;
-		--10khz时钟信号
+		--1khz时钟信号
 		nsec: out std_logic
 		--秒
 	);
@@ -14,7 +14,7 @@ end f2sec;
 
 architecture f2sec_bh of f2sec is
 
-signal tmp_sec : integer range 0 to 9999;
+signal tmp_sec : integer range 0 to 999;
 
 begin
 	process(clk)
@@ -22,7 +22,7 @@ begin
 		if(clk'event and clk = '1') then
 			tmp_sec <= tmp_sec + 1;
 		end if;
-		if(tmp_sec < 2000) then
+		if(tmp_sec < 200) then
 			nsec <= '1';
 		else
 			nsec <= '0';

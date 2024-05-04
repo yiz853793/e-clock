409815b7f7c5ec78ee1e13ba0643f938ae1371cd
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-05-04 11:28:21 +0800

    Update num_display.vhd

--- num_display.vhd
+++ num_display.vhd
@@ -6,6 +6,7 @@ entity num_display is
 	port(
 		en: in std_logic;
 		--类似使能端，='0' 输出num，='1'输出空
+		zero : in std_logic_vector(3 downto 0);
 		numh, numl: in std_logic_vector(3 downto 0);
 		--输入高四位和低四位
 		dish, disl: out std_logic_vector(3 downto 0)
@@ -15,6 +16,6 @@ end num_display;
 
 architecture num_display_bh of num_display is
 begin
-	dish <= numh when en = '0' else "1111";
-	disl <= numl when en = '0' else "1111";
+	dish <= numh when en = '0' else zero;
+	disl <= numl when en = '0' else zero;
 end num_display_bh;

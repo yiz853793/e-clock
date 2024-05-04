65bf8c27c7f15829db0de94367c12fbf8543ca85
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-05-04 10:08:13 +0800

    Update dividerOfRing.vhd

--- dividerOfRing.vhd
+++ dividerOfRing.vhd
@@ -10,29 +10,27 @@ entity dividerOfRing is
 		enable: in std_logic; --使能信号
 		clr: in std_logic; -- 异步清零
 		N : in integer;  --分频大小
-		newFre: out std_logic -- 分频后时钟频率
+		newFre: inout std_logic -- 分频后时钟频率
 	);
 end entity;
 
 architecture behavioral of dividerOfRing is
 signal ClkCnt : integer range 0 to 1;
-signal CLK : std_logic := '0';
 begin
     process(highFre, enable, clr)
     begin
 		if(enable = '1') then
 			if (clr = '1') then
-				CLK <= '0';
+				newFre <= '0';
 				ClkCnt <= 0;
 			elsif (highFre'event and highFre = '1') then
 				if (ClkCnt = (N - 1)) then
 					ClkCnt <= 0;
-					CLK <= not CLK;
+					newFre <= not newFre;
 				else
 					ClkCnt <= ClkCnt + 1;
 				end if;
 			end if;
 		end if;
     end process;
-	newFre <= CLK;
 end behavioral;

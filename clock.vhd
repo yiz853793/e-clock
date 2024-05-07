4b31772e63b9a3f50b025c7b0c31faeb91e2111b
floation-cutie <wzxFloation_cutie@bupt.edu.cn>
2024-05-07 10:36:39 +0800

    modify isspark signal in another form

--- clock.vhd
+++ clock.vhd
@@ -309,8 +309,8 @@ begin
 	--mode(3) = '1', show_alert = '1'，QD调整时针
 
 	isspark <= '1' when (en_clock = '1' and mode(0) = '1' and t_hourh = a_hourh and t_hourl = a_hourl and t_minh = a_minh
-	and t_minl = a_minl and t_sech = a_sech and t_secl = a_secl) else
-		mcarry and scarry and en_clock;
+	and t_minl = a_minl and t_sech = a_sech and t_secl = a_secl) or (mcarry = '0' and scarry = '0' and en_clock = '1') else
+		'0';
 	--蜂鸣器信号
 	
 	ring_alert: ring port map(isspark, sec, enlow, enhigh);

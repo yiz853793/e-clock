65566bdb513a42b9c5b3be6b7e3f0d3a134225b0
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-05-09 17:19:21 +0800

    Update f2f.vhd

--- f2f.vhd
+++ f2f.vhd
@@ -5,24 +5,24 @@ use ieee.std_logic_unsigned.all;
 entity f2f is
 	port(
 		clk:in std_logic; --1kHz
-		f50:out std_logic --50Hz
+		f50:out std_logic --100Hz
 	);
 end f2f;
 
 architecture f2f_bh of f2f is
-signal cnt: integer range 0 to 19; --计数器，来实现消抖的关键，一般的按键的延时是在20ms左右
+signal cnt: integer range 0 to 9; --计数器，来实现消抖的关键，一般的按键的延时是在20ms左右
 begin
 	process(clk)
 	begin
 		if (clk'event and clk='1') then 
 			cnt <= cnt + 1;
-			if (cnt = 0) then 
+			if(cnt = 9)then
+				cnt <= 0;
+			end if;
+			if cnt = 0 then 
 				f50 <= '1';
 			else
 				f50 <= '0';
-			end if;
-			if (cnt = 19) then 
-				cnt <= 0;
 			end if;
 		end if;
 	end process;

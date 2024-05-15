3cb00f8d4820beeed90c068b02b8f07cb5d14565
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-05-15 10:56:03 +0800

    Update num_display.vhd

--- num_display.vhd
+++ num_display.vhd
@@ -9,16 +9,16 @@ entity num_display is
 
 		numh:in std_logic_vector(2 downto 0);
 		numl: in std_logic_vector(3 downto 0);
-		--输入高四位和低四位
-		dish: out std_logic_vector(2 downto 0);
-		disl: out std_logic_vector(3 downto 0)
+		--输入高三位和低四位
+
+		dish, disl: out std_logic_vector(3 downto 0)
 		--输出高四位和低四位
 	);
 end num_display;
 
 architecture num_display_bh of num_display is
 begin
-	dish <= numh when en = '0' else "111";
+	dish <= '0' & numh when en = '0' else "1111";
 	disl <= numl when en = '0' else "1111";
 end num_display_bh;
 

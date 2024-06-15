de5ba240dbc5cec3b31750d86746ae930c4d0cc9
yiz853793 <119387455+yiz853793@users.noreply.github.com>
2024-06-16 02:05:06 +0800

    Update ring.vhd
    增加紧音

--- ring.vhd
+++ ring.vhd
@@ -8,6 +8,7 @@ entity ring is
         enable: in std_logic;
         clk: in std_logic;
         --1Hz时钟信号
+	enclock: in std_logic;
         enlow: out std_logic;
         --低音C使能信号，高电平有效
         enhigh: out std_logic
@@ -22,25 +23,24 @@ begin
     process(enable, clk)
     begin
         if (clk'event and clk = '1') then
-			if (count < 2) then
-				--前两秒低音
-				count <= count + 1;
-				enlow <= '1';
-			elsif (count < 4) then
-				--之后两秒高音
-				count <= count + 1;
-				enlow <= '0';
-				enhigh <= '1';
+	    if(enclock = '0') then
+		enlow <= '0';
+		enhigh <= '0';
+	    	count <= "0111";
+	    elsif (count < 2) then
+		--前两秒低音
+		count <= count + 1;
+		enlow <= '1';
+	    elsif (count < 4) then
+		--之后两秒高音
+		count <= count + 1;
+		enlow <= '0';
+		enhigh <= '1';
             elsif (count < 6) then
                 --之后两秒低音
                 count <= count + 1;
 				enlow <= '1';
                 enhigh <= '0';
-            elsif (count < 7) then
-                --最后一秒高音
-                count <= count + 1;
-				enlow <= '0';
-				enhigh <= '1';
             else
                 --第七秒后禁用低音和高音
                 enlow <= '0';
